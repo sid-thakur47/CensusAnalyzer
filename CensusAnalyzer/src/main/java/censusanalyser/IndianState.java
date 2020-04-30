d14ca7f793b4f4407b2df3aa271e361bfd3edbd5
sid-thakur47 <sidthakur258@gmail.com>
2020-04-30 10:42:02 +0530

    ADDED:ToString method

--- CensusAnalyzer/src/main/java/censusanalyser/IndianState.java
+++ CensusAnalyzer/src/main/java/censusanalyser/IndianState.java
@@ -3,9 +3,17 @@ package censusanalyser;
 import com.opencsv.bean.CsvBindByName;
 
 public class IndianState {
+
     @CsvBindByName(column = "State", required = true)
     public String state;
 
     @CsvBindByName(column = "StateCode", required = true)
     public String stateCode;
+    @Override
+    public String toString() {
+        return "IndianState{" +
+                "state='" + state + '\'' +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
 }
