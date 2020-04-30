1bcb1bd9441b99e481b45da15ad15626b3776706
sid-thakur47 <sidthakur258@gmail.com>
2020-04-30 10:29:15 +0530

    ADDED:Test to check record of state matches

--- CensusAnalyzer/src/test/java/censusanalyser/CensusAnalyserTest.java
+++ CensusAnalyzer/src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,9 +9,10 @@ public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE = "./src/test/resources/IndiaStateCensusData.doc";
+    private static final String INDIA_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
 
     @Test
-    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+    public void givenIndianCensus_CSVFile_ReturnsCorrectRecords() {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData( INDIA_CENSUS_CSV_FILE_PATH );
@@ -31,12 +32,22 @@ public class CensusAnalyserTest {
         }
     }
     @Test
-    public void givenIndianCensusData_WithWrongFileType_Should_RETURNEXCEPTION() {
+    public void givenIndianCensusData_WithWrongFileType_Should_ReturnException() {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             censusAnalyser.loadIndiaCensusData( WRONG_CSV_FILE_TYPE );
         } catch (CensusAnalyserException e) {
             Assert.assertEquals( CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type );
+        }
+    }
+
+    @Test
+    public void givenIndianStateCode_CSVFile_ReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndianStateCodeData( INDIA_STATE_CSV_FILE_PATH );
+            Assert.assertEquals( 36, numOfRecords );
+        } catch (CensusAnalyserException e) {
         }
     }
 }
