8c9104cddc343a2364366bf404e9c91675804169
sid-thakur47 <sidthakur258@gmail.com>
2020-04-30 18:23:02 +0530

    MODIFIED:CSV loading method

--- CensusAnalyzer/src/test/java/censusanalyser/CensusAnalyserTest.java
+++ CensusAnalyzer/src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -5,11 +5,10 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
-
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE = "./src/test/resources/IndiaStateCensusData.doc";
-    private static final String INDIA_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
+    private static final String INDIA_STATE_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
     private static final String WRONG_STATE_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
     private static final String WRONG_STATE_CSV_FILE_TYPE = "./src/test/resources/IndiaStateCode.doc";
 
@@ -48,7 +47,7 @@ public class CensusAnalyserTest {
         try {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndianStateCodeData( INDIA_STATE_CSV_FILE_PATH );
-            Assert.assertEquals( 36, numOfRecords );
+            Assert.assertEquals( 37, numOfRecords );
         } catch (CensusAnalyserException e) {
         }
     }
