ec7047438230f8accbc1c5541830462e2d375cdf
sid-thakur47 <sidthakur258@gmail.com>
2020-05-07 12:15:32 +0530

    ADDED:Functionality to use Reflection instead of if else in loading Census data

--- CensusAnalyzer/src/main/java/censusanalyser/service/CSVFieldSorter.java
+++ CensusAnalyzer/src/main/java/censusanalyser/service/CSVFieldSorter.java
@@ -1,14 +1,17 @@
 package censusanalyser.service;
 
-import censusanalyser.dao.IndiaCensusDAO;
+import censusanalyser.dao.CensusDAO;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Comparator;
+import java.util.Map;
 
 public class CSVFieldSorter {
 
-    public Comparator<IndiaCensusDAO> getCurrentSort(String field) {
+    public Comparator<CensusDAO> getCurrentSort(String field) {
 
-        Comparator<IndiaCensusDAO> comparator = null;
+        Comparator<CensusDAO> comparator = null;
         switch (field) {
             case "population":
                 comparator = Comparator.comparing( census -> census.population );
@@ -27,5 +30,17 @@ public class CSVFieldSorter {
                 break;
         }
         return comparator;
+    }
+
+    public <E> void getCensusObject(Class<E> censusClass, Map censusMap, Object censusCSV) {
+        try {
+            Field field = censusClass.getDeclaredField( "state" );
+            Class<?> censusDAOClass = Class.forName( "censusanalyser.dao.CensusDAO" );
+            Constructor<?> censusConstructor = censusDAOClass.getConstructor( censusClass );
+            String value = (String) field.get( censusCSV );
+            censusMap.put( value, censusConstructor.newInstance( censusCSV ) );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
